96bf2a657a0bb52bdf3bccc76bf53e1c495b8cb0
Haze272 <studiorain27@gmail.com>
2022-09-19 12:29:45 +0300

    Убраны лишние .js файлы

--- (1) HelloWorld/app.js
+++
@@ -1,9 +0,0 @@
-var World = /** @class */ (function () {
-    function World(_name) {
-        this.name = _name;
-    }
-    return World;
-}());
-var ourWorld = new World("Мир");
-var header = this.document.getElementById("header");
-header.innerHTML = "Привет, " + ourWorld.name;
